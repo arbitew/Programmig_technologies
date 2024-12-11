2191b310a52292449c4b58733807cf991dd11d97
arbitew <121195731+arbitew@users.noreply.github.com>
2024-12-11 21:31:22 +0300

    Add files via upload

---
+++ Lab_7/Lab_7/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Lab_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.XplZpl = new System.Windows.Forms.Button();
+            this.XmZm = new System.Windows.Forms.Button();
+            this.StopB = new System.Windows.Forms.Button();
+            this.CW_Xm = new System.Windows.Forms.Button();
+            this.CCW_Xpl = new System.Windows.Forms.Button();
+            this.XTZX = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SaveB = new System.Windows.Forms.Button();
+            this.ExitB = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.SuspendLayout();
+            // 
+            // XplZpl
+            // 
+            this.XplZpl.Location = new System.Drawing.Point(117, 18);
+            this.XplZpl.Name = "XplZpl";
+            this.XplZpl.Size = new System.Drawing.Size(100, 100);
+            this.XplZpl.TabIndex = 0;
+            this.XplZpl.Text = "X+";
+            this.XplZpl.UseVisualStyleBackColor = true;
+            this.XplZpl.Click += new System.EventHandler(this.XplZpl_Click);
+            // 
+            // XmZm
+            // 
+            this.XmZm.Location = new System.Drawing.Point(117, 230);
+            this.XmZm.Name = "XmZm";
+            this.XmZm.Size = new System.Drawing.Size(100, 100);
+            this.XmZm.TabIndex = 1;
+            this.XmZm.Text = "X-";
+            this.XmZm.UseVisualStyleBackColor = true;
+            this.XmZm.Click += new System.EventHandler(this.XmZm_Click);
+            // 
+            // StopB
+            // 
+            this.StopB.Location = new System.Drawing.Point(117, 124);
+            this.StopB.Name = "StopB";
+            this.StopB.Size = new System.Drawing.Size(100, 100);
+            this.StopB.TabIndex = 2;
+            this.StopB.Text = "Stop";
+            this.StopB.UseVisualStyleBackColor = true;
+            this.StopB.Click += new System.EventHandler(this.StopB_Click);
+            // 
+            // CW_Xm
+            // 
+            this.CW_Xm.Location = new System.Drawing.Point(11, 124);
+            this.CW_Xm.Name = "CW_Xm";
+            this.CW_Xm.Size = new System.Drawing.Size(100, 100);
+            this.CW_Xm.TabIndex = 3;
+            this.CW_Xm.Text = "CW";
+            this.CW_Xm.UseVisualStyleBackColor = true;
+            this.CW_Xm.Click += new System.EventHandler(this.CW_Xm_Click);
+            // 
+            // CCW_Xpl
+            // 
+            this.CCW_Xpl.Location = new System.Drawing.Point(223, 124);
+            this.CCW_Xpl.Name = "CCW_Xpl";
+            this.CCW_Xpl.Size = new System.Drawing.Size(100, 100);
+            this.CCW_Xpl.TabIndex = 4;
+            this.CCW_Xpl.Text = "CCW";
+            this.CCW_Xpl.UseVisualStyleBackColor = true;
+            this.CCW_Xpl.Click += new System.EventHandler(this.CCW_Xpl_Click);
+            // 
+            // XTZX
+            // 
+            this.XTZX.Location = new System.Drawing.Point(493, 178);
+            this.XTZX.Name = "XTZX";
+            this.XTZX.Size = new System.Drawing.Size(100, 100);
+            this.XTZX.TabIndex = 5;
+            this.XTZX.Text = "XT";
+            this.XTZX.UseVisualStyleBackColor = true;
+            this.XTZX.Click += new System.EventHandler(this.XTZX_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(493, 37);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(164, 92);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "X        0\nTheta 0\nZ        0\n";
+            // 
+            // SaveB
+            // 
+            this.SaveB.Location = new System.Drawing.Point(599, 178);
+            this.SaveB.Name = "SaveB";
+            this.SaveB.Size = new System.Drawing.Size(108, 46);
+            this.SaveB.TabIndex = 8;
+            this.SaveB.Text = "Save";
+            this.SaveB.UseVisualStyleBackColor = true;
+            this.SaveB.Click += new System.EventHandler(this.SaveB_Click);
+            // 
+            // ExitB
+            // 
+            this.ExitB.Location = new System.Drawing.Point(599, 232);
+            this.ExitB.Name = "ExitB";
+            this.ExitB.Size = new System.Drawing.Size(108, 46);
+            this.ExitB.TabIndex = 9;
+            this.ExitB.Text = "Exit";
+            this.ExitB.UseVisualStyleBackColor = true;
+            this.ExitB.Click += new System.EventHandler(this.ExitB_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(451, 353);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 10;
+            this.progressBar1.Value = 50;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(583, 370);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(134, 42);
+            this.hScrollBar1.TabIndex = 11;
+            this.hScrollBar1.Value = 50;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.ExitB);
+            this.Controls.Add(this.SaveB);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.XTZX);
+            this.Controls.Add(this.CCW_Xpl);
+            this.Controls.Add(this.CW_Xm);
+            this.Controls.Add(this.StopB);
+            this.Controls.Add(this.XmZm);
+            this.Controls.Add(this.XplZpl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button XplZpl;
+        private System.Windows.Forms.Button XmZm;
+        private System.Windows.Forms.Button StopB;
+        private System.Windows.Forms.Button CW_Xm;
+        private System.Windows.Forms.Button CCW_Xpl;
+        private System.Windows.Forms.Button XTZX;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button SaveB;
+        private System.Windows.Forms.Button ExitB;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+    }
+}
+
---
+++ Lab_7/Lab_7/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string rB_Change(string a, string b)
+        {
+            string x = "";
+            string w = "";
+            string z = "";
+            int d = 0;
+            if (b == "X+")
+            {
+                //string a = richTextBox1.Text;
+                for (int i = 1; i < a.Length; i++)
+                {
+                    if ((Char.IsNumber(a[i])) || a[i] == '-')
+                    {
+                        while (a[i] != ' ' && a[i] != '\n' && i != (a.Length - 1))
+                        {
+                            if (d == 0)
+                            {
+                                x += a[i];
+                            }
+                            if (d == 1)
+                            {
+                                w += a[i];
+                            }
+                            if (d == 2)
+                            {
+                                z += a[i];
+                            }
+                            i++;
+                        }
+                        d++;
+                    }
+                }
+                return "X" + "       " + (Convert.ToDouble(x) + 0.1).ToString() + "\n" + "Theta" + w + "\n" + "Z" + "       " + z + "\n";
+            }
+            else if(b == "X-")
+            {
+                for (int i = 1; i < a.Length; i++)
+                {
+                    if ((Char.IsNumber(a[i])) || a[i] == '-')
+                    {
+                        while (a[i] != ' ' && a[i] != '\n' && i != (a.Length - 1))
+                        {
+                            if (d == 0)
+                            {
+                                x += a[i];
+                            }
+                            if (d == 1)
+                            {
+                                w += a[i];
+                            }
+                            if (d == 2)
+                            {
+                                z += a[i];
+                            }
+                            i++;
+                        }
+                        d++;
+                    }
+                }
+                return "X" + "       " + (Convert.ToDouble(x) - 0.1).ToString() + "\n" + "Theta" + w + "\n" + "Z" + "       " + z + "\n";
+            }
+            else if (b == "Z+")
+            {
+                for (int i = 1; i < a.Length; i++)
+                {
+                    if ((Char.IsNumber(a[i])) || a[i] == '-')
+                    {
+                        while (a[i] != ' ' && a[i] != '\n' && i != (a.Length - 1))
+                        {
+                            if (d == 0)
+                            {
+                                x += a[i];
+                            }
+                            if (d == 1)
+                            {
+                                w += a[i];
+                            }
+                            if (d == 2)
+                            {
+                                z += a[i];
+                            }
+                            i++;
+                        }
+                        d++;
+                    }
+                }
+                return "X" + "       " + x + "\n" + "Theta" + w + "\n" + "Z" + "       " + (Convert.ToDouble(z) + 0.1).ToString() + "\n";
+            }
+            else if (b == "Z-")
+            {
+                for (int i = 1; i < a.Length; i++)
+                {
+                    if ((Char.IsNumber(a[i])) || a[i] == '-')
+                    {
+                        while (a[i] != ' ' && a[i] != '\n' && i != (a.Length - 1))
+                        {
+                            if (d == 0)
+                            {
+                                x += a[i];
+                            }
+                            if (d == 1)
+                            {
+                                w += a[i];
+                            }
+                            if (d == 2)
+                            {
+                                z += a[i];
+                            }
+                            i++;
+                        }
+                        d++;
+                    }
+                }
+                return "X" + "       " + x + "\n" + "Theta" + w + "\n" + "Z" + "       " + (Convert.ToDouble(z) - 0.1).ToString() + "\n";
+            }
+            else if (b == "CCW")
+            {
+                for (int i = 1; i < a.Length; i++)
+                {
+                    if ((Char.IsNumber(a[i])) || a[i] == '-')
+                    {
+                        while (a[i] != ' ' && a[i] != '\n' && i != (a.Length - 1))
+                        {
+                            if (d == 0)
+                            {
+                                x += a[i];
+                            }
+                            if (d == 1)
+                            {
+                                w += a[i];
+                            }
+                            if (d == 2)
+                            {
+                                z += a[i];
+                            }
+                            i++;
+                        }
+                        d++;
+                    }
+                }
+                return "X" + "       " + x + "\n" + "Theta" + (Convert.ToDouble(w) + 0.1).ToString() + "\n" + "Z" + "       " + z + "\n";
+            }
+            else if (b == "CW")
+            {
+                for (int i = 1; i < a.Length; i++)
+                {
+                    if ((Char.IsNumber(a[i])) || a[i] == '-')
+                    {
+                        while (a[i] != ' ' && a[i] != '\n' && i != (a.Length - 1))
+                        {
+                            if (d == 0)
+                            {
+                                x += a[i];
+                            }
+                            if (d == 1)
+                            {
+                                w += a[i];
+                            }
+                            if (d == 2)
+                            {
+                                z += a[i];
+                            }
+                            i++;
+                        }
+                        d++;
+                    }
+                }
+                return "X" + "       " + x + "\n" + "Theta" + (Convert.ToDouble(w) - 0.1).ToString() + "\n" + "Z" + "       " + z + "\n";
+            }
+            return "";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void XTZX_Click(object sender, EventArgs e)
+        {
+            if(XTZX.Text == "XT")
+            {
+                XTZX.Text = "ZT";
+                XplZpl.Text = "Z+";
+                XmZm.Text = "Z-";
+                CCW_Xpl.Text = "X+";
+                CW_Xm.Text = "X-";
+            }
+            else
+            {
+                XTZX.Text = "XT";
+                XplZpl.Text = "X+";
+                XmZm.Text = "X-";
+                CCW_Xpl.Text = "CCW";
+                CW_Xm.Text = "CW";
+            }
+        }
+
+        private void XplZpl_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = 
+                
+                
+                 rB_Change(richTextBox1.Text, XplZpl.Text);
+        }
+
+
+
+        private void XmZm_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = rB_Change(richTextBox1.Text, XmZm.Text);
+        }
+
+        
+
+        private void ExitB_Click(object sender, EventArgs e)
+        {
+                
+            DialogResult result = MessageBox.Show(
+                "Save?",
+                "Exit",              
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+            if (result == DialogResult.Yes)
+            {
+                SaveB_Click(sender, e);
+            }
+            Application.Exit();           
+        }
+
+        private void CW_Xm_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = rB_Change(richTextBox1.Text, CW_Xm.Text);
+        }
+        private void SaveB_Click(object sender, EventArgs e)
+        {
+            string path;
+            int num = 1;
+            while (System.IO.File.Exists("G" + Convert.ToString(num) + ".dat"))
+            {
+                path = "G" + Convert.ToString(num) + ".dat";
+                num++;
+            }
+            path = "G" + Convert.ToString(num) + ".dat";
+            System.IO.File.WriteAllText(path, richTextBox1.Text);
+        }
+        private void CCW_Xpl_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = rB_Change(richTextBox1.Text, CCW_Xpl.Text);
+        }
+
+        
+        private void StopB_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "X" + "       " + "0" + "\n" + "Theta" + "0" + "\n" + "Z" + "       " + "0" + "\n";
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            XplZpl.Location = new Point(hScrollBar1.Value, 42) ;
+        }
+    }
+}
